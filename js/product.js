5c4fc7c5ef2ce917ec01cee9d40dffbbbd72c79c
biboamy <biboamybibo@gmail.com>
2016-02-13 23:58:23 -0500

    2/13

---
+++ js/product.js
@@ -0,0 +1,7 @@
+$(".order_btn").mouseover(function() {
+    $(this).css("background-color", "#dac997");
+    $(this).find("a").css("color", "black");
+}).mouseout(function() {
+    $(this).css("background-color", "transparent");
+    $(this).find("a").css("color", "#dac997");
+});
